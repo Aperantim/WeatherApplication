471ec87502ce718f036fb103aed5499de7060499
Vitaliy Nesterov <liyuop@mail.ru>
2020-02-28 18:10:09 +0300

    Built the interface. Added 2 files: current weather model and weather icon manager. Added fake data to try application

--- WeatherApplication/ViewController.swift
+++ WeatherApplication/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var appearentTempertatureLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    
+    @IBAction func refreshButtonTapped(_ sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let icon = WeatherIconManager.Rain.image
+        let currentWeather = CurrentWeather(temperature: 10.0, apparentTemperature: 5.0, humidity: 30, pressure: 750, icon: icon)
+        updateUIWithCurrentWeather(currentWeather: currentWeather)
     }
-
-
+    
+    func updateUIWithCurrentWeather(currentWeather: CurrentWeather) {
+        
+        self.imageView.image = currentWeather.icon
+        self.pressureLabel.text = currentWeather.pressureString
+        self.temperatureLabel.text = currentWeather.temperatureString
+        self.appearentTempertatureLabel.text = currentWeather.apparentTemperatureString
+        self.humidityLabel.text = currentWeather.humidityString
+        
+    }
 }
 
