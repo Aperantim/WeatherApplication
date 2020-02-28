e3e1b851b5702ee3b03d778c44817abc52138df1
Vitaliy Nesterov <liyuop@mail.ru>
2020-02-28 18:00:21 +0300

    Initial Commit

---
+++ WeatherApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApplication
+//
+//  Created by Виталий on 28.02.2020.
+//  Copyright © 2020 Aperantim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
