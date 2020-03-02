fbdfa47d362855055d268c5a37100d514726db1a
Vitaliy Nesterov <liyuop@mail.ru>
2020-03-02 18:56:50 +0300

    Work with API, app is almost complete

---
+++ WeatherApplication/ErrorManager.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorManager.swift
+//  WeatherApplication
+//
+//  Created by Виталий on 28.02.2020.
+//  Copyright © 2020 Aperantim. All rights reserved.
+//
+
+import Foundation
+
+public let APEnetworkingErrorDomain = "ru.aperantim.WeatherApplication.NetworkingError"
+public let MissingHTTPResponseError = 100
--- WeatherApplication/ViewController.swift
+++ WeatherApplication/ViewController.swift
@@ -17,18 +17,43 @@ class ViewController: UIViewController {
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var appearentTempertatureLabel: UILabel!
     @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     
     @IBAction func refreshButtonTapped(_ sender: UIButton) {
-        
+        toogleActivityIndicator(on: true)
+        getCurrentWeatherData()
     }
+    
+    func toogleActivityIndicator(on: Bool) {
+        refreshButton.isHidden = on
+        if on {
+            activityIndicator.startAnimating()
+        } else {
+            activityIndicator.stopAnimating()
+        }
+    }
+    
+    lazy var weatherManager = APIWeatherManager(apiKey: "6b264b0362fc7e62029c39e8d782437b")
+    let coordinates = Coordinates(latitude: 59.933027, longitude: 30.335890)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        getCurrentWeatherData()
         
-        let icon = WeatherIconManager.Rain.image
-        let currentWeather = CurrentWeather(temperature: 10.0, apparentTemperature: 5.0, humidity: 30, pressure: 750, icon: icon)
-        updateUIWithCurrentWeather(currentWeather: currentWeather)
+    }
+    
+    func getCurrentWeatherData() {
+        weatherManager.fetchCurrentWeatherWith(coordinates: coordinates) { (result) in
+            self.toogleActivityIndicator(on: false)
+            
+            switch result {
+            case .Success(let currentWeather):
+                self.updateUIWithCurrentWeather(currentWeather: currentWeather)
+            case .Failure(let error as NSError):
+                self.alertError(title: "Unable to get data", message: "\(error.localizedDescription)", error: error)
+            }
+        }
     }
     
     func updateUIWithCurrentWeather(currentWeather: CurrentWeather) {
@@ -39,6 +64,14 @@ class ViewController: UIViewController {
         self.appearentTempertatureLabel.text = currentWeather.apparentTemperatureString
         self.humidityLabel.text = currentWeather.humidityString
         
+    }
+    
+    func alertError(title: String, message: String, error: NSError) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(okButton)
+        
+        present(alertController, animated: true)
     }
 }
 
